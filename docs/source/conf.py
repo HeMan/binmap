a2e1d9bdc3bf67c4a8b3f74576d493332e095ff1
Jimmy Hedman <jimmy.hedman@gmail.com>
2020-11-08 21:52:08 +0100

    Remove typehints requirement.
    - Due to some problem with readthedocs I'll try to remove autodoc
      typehints package.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,6 @@ master_doc = "index"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx_rtd_theme",
-    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
